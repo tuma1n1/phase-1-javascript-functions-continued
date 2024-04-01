63eb717361c5cedff31cd1575cb75f0c53915941
Tumaini <tumainikebaya.234@gmail.com>
2024-04-01 12:06:00 +0300

    Adds index.js

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // code your solution here
+
+const saturdayFun = function(activity = "roller-skate"){
+    
+    return ("This Saturday, I want to " + activity + "!");
+}
+console.log(saturdayFun("Swimm"));
+
+const mondayWork = function(activity = "go to the office"){
+
+    return ("This Monday, I will " + activity + ".")
+    
+}
+console.log(mondayWork("work from home"));
+
+
+
+
+function wrapAdjective(flair = "*"){
+    
+    return function(complement = "special"){
+        return "You are " + flair + complement + flair + "!";
+    }
+}
+
+console.log(wrapAdjective("*")("a hard worker")("*"));
